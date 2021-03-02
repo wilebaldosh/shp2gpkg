0e2f4c108e0af3f620c4e195a8017af7910f7185
Wilebaldo Santana Hernández <wisahe@gmail.com>
2021-03-02 14:04:01 -0700

    Relocalización de entrada y salida de archivos

--- shp_to_gpkg.R
+++ shp_to_gpkg.R
@@ -9,6 +9,7 @@ gpkg_path <- "data/gpkg/"
 
 # Seleccionamos todos los que tengan extensión '.shp'
 archivos = list.files(path = shp_path, pattern = '.shp$')
+
 # Les quitamos la extensión: '.shp'
 nombres = str_remove(archivos, '.shp')
 
@@ -17,15 +18,14 @@ nombres = str_remove(archivos, '.shp')
 for(i in 1:length(archivos)) {
   # Importante leer bajo la codificación 'WINDOWS-1252',
   # para evitar caracteres extraños en los nombres de los municipios
-  # y localidades
-  x = st_read(archivos[i], options ="ENCODING=WINDOWS-1252")
+  # y localidades como: "Amatl�n de Ca�as", ahora deberá aparecer
+  # como "Amatlán de Cañas", entre otros.
+  x = st_read(paste0(shp_path, archivos[i]), options ="ENCODING=WINDOWS-1252")
+
   # Pegamos la ruta y el nombre del archivo, sin extensión,
   # para agregarle la extensión '.gpkg'
-  archivo_gpkg <- paste0(paste0(ruta,nombres[i]), '.gpkg')
+  archivo_gpkg <- paste0(paste0(gpkg_path, nombres[i]), '.gpkg')
+
   # Escribimos el archivo GPKG
   st_write(x, archivo_gpkg)
 }
-
-# Probamos la lectura con uno de los mapas. Ya no debería tener 
-# caracteres raros, como "Amatl�n de Ca�as", ahora deberá aparecer
-# como "Amatlán de Cañas", entre otros
